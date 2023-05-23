259afa15f3becc970e7108d90bd7b5fe7d40d6df
Oleksandr Padko <sasha.pad@gmail.com>
2023-05-23 00:05:24 -0700

    Add initial code

---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+
+import quart
+import quart_cors
+from quart import request
+
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+
+@app.post("/generate_meme")
+async def generate_meme():
+    request = await quart.request.get_json(force=True)
+    print("===")
+    print(request["memeText"])
+    print(request["memeTemplateName"])
+    if "memeUseCase" in request.keys():
+        print(request["memeUseCase"])
+    return quart.Response(response='OK', status=200)
+
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    host = request.headers['Host']
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)
+
+if __name__ == "__main__":
+    main()
